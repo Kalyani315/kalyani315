2bc89d5fef1c3b203e441f3ca0d55b948956c6e5
Kalyani315 <kalyaniyeraguti@gmail.com>
2024-11-02 12:19:43 +0530

    Update PurchaseHistory.jsx

--- src/PurchaseHistory.jsx
+++ src/PurchaseHistory.jsx
@@ -3,6 +3,7 @@ function PurchaseHistory()
     return(
         <>
         <h1>this is modified PurchaseHistory page</h1>
+            <h1> done by someone</h1>
         <h1>This is PurchaseHistory page </h1>
         </>
     )
